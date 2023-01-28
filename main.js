71fa62ce58f0267033e6a53a4f4b20b3f17462f0
小夜 <1035017465@qq.com>
2023-01-28 21:16:45 +0800

    1.0.2添加保存功能

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // main.js
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const fs = require('fs');
 
 function createWindow() {
   let window = new BrowserWindow({
@@ -16,28 +17,34 @@ function createWindow() {
   // 设置window鼠标穿透
   window.setIgnoreMouseEvents(true);
 
+  // 读取red-package.json文件
+  let redPackage = fs.readFileSync(path.join(__dirname, 'red-package.json'), 'utf-8');
+  // 将red-package.json文件转换为json对象
+  redPackage = JSON.parse(redPackage);
+  // 设置窗口的位置，left为rePackage.position[0]，top为redPackage.position[1]
+  window.setPosition(redPackage.position[0], redPackage.position[1]);
+
+
   // 监听键盘的ctrl+shift+o组合键，实现窗口的显示和隐藏
-  require('electron').globalShortcut.register('ctrl+shift+o', () => {
-    console.log('ctrl+shift+o');
-    // 判断win是否显示
-    if (window.isVisible()) {
-      // 隐藏win
-      window.hide();
-    } else {
-      // 显示win
-      window.show();
-    }
-  });
+  showO(window);
 
-  // 监听键盘的ctrl+shift+p组合键
-  require('electron').globalShortcut.register('ctrl+shift+p', () => {
-    console.log('ctrl+shift+p');
-    // 修改win的位置，使其在屏幕正中间
-    // 获取屏幕的宽高
-    let { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
-    window.setPosition(width / 2 - 200, height / 2 - 200);
-  });
+  // 监听键盘的ctrl+shift+p组合键,居中显示
+  autoP(window);
 
+  //  上下左右移动
+  UDLR(window);
+
+  // 监听屏幕分辨率变化，重新设置窗口位置
+  resolution(window);
+
+  // 保存窗口位置
+  savePosition(window);
+
+}
+
+
+// 上下左右移动
+function UDLR(window) {
   // 监听键盘的ctrl+shift+上/下/左/右组合键
   require('electron').globalShortcut.register('ctrl+shift+up', () => {
     console.log('ctrl+shift+up1');
@@ -70,14 +77,65 @@ function createWindow() {
     // 修改win的位置
     window.setPosition(position[0] + 1, position[1]);
   });
-  // 监听屏幕分辨率变化，重新设置窗口位置
+}
+
+// 监听屏幕分辨率变化，重新设置窗口位置
+function resolution(window) {
   require('electron').screen.on('display-metrics-changed', () => {
     // 获取屏幕的宽高
     let { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
     window.setPosition(width / 2 - 200, height / 2 - 200);
   }
   );
-
 }
 
+// 显示隐藏
+function showO(window) {
+  require('electron').globalShortcut.register('ctrl+shift+o', () => {
+    console.log('ctrl+shift+o');
+    // 判断win是否显示
+    if (window.isVisible()) {
+      // 隐藏win
+      window.hide();
+    } else {
+      // 显示win
+      window.show();
+    }
+  });
+}
+
+// 居中显示
+function autoP(window) {
+  require('electron').globalShortcut.register('ctrl+shift+p', () => {
+    console.log('ctrl+shift+p');
+    // 打印窗口位置
+    console.log(window.getPosition());
+    // 修改win的位置，使其在屏幕正中间
+    // 获取屏幕的宽高
+    let { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
+    window.setPosition(width / 2 - 200, height / 2 - 200);
+    // 打印窗口位置
+    console.log(window.getPosition());
+  });
+}
+
+// 保存窗口位置
+function savePosition(window) {
+  // 将red-package.json文件中的position属性修改为当前窗口的位置
+  // 监听ctrl+shift+l组合键
+  require('electron').globalShortcut.register('ctrl+shift+i', () => {
+    console.log('ctrl+shift+i');
+    // 获取当前窗口的位置
+    let position = window.getPosition();
+    // 将red-package.json文件中的position属性修改为当前窗口的位置
+    let redPackage = require('./red-package.json');
+    redPackage.position = position;
+
+    // 将red-package.json文件中的position属性修改为当前窗口的位置
+    fs.writeFileSync('./red-package.json', JSON.stringify(redPackage));
+  }
+  );
+
+
+}
 app.on('ready', createWindow);
