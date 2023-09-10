68217d70fe17433063433f5582cffc9f3dea2896
小夜 <97166212+xiaoye6688@users.noreply.github.com>
2023-09-11 00:20:38 +0800

    123

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function createWindow() {
     skipTaskbar: true,// 任务栏中是否隐藏 true为隐藏 false为显示
     icon: path.join(__dirname, 'icon/add.png'),
   });
+  
 
   window.loadURL(path.join('file://', __dirname, 'index.html'));
   // 设置window鼠标穿透
